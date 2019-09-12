a0fd805491a670fa27a737a839a102989d925161
Ken Mitchell <kmitch95120@gmail.com>
2019-09-11 17:31:46 -0700

    Added ATOM Lua Module + enable/disable NC scripts

---
+++ ReaScript/ATOM_enableNC.lua
@@ -0,0 +1,23 @@
+
+-- This code needs to be at the top of every ATOM script.
+-- Otherwise, we won't be able to find our support module. 
+local path = ({reaper.get_action_context()})[2]:match('^.+[\\//]')
+package.path = path .. "?.lua"
+atom = require("ATOM")
+
+atom.debug("disable") -- Turn OFF debug messages
+
+atom.dbg_msg("\nTrying to locate an ATOM Pad Controller\n")
+
+atom.find() -- outID is deleted if device not found
+
+if reaper.HasExtState("ATOM", "outID") then -- Should have been set by find() 
+  target_outID = tonumber(reaper.GetExtState("ATOM", "outID"))
+  atom.dbg_msg("\ntarget_outID: " .. tostring(target_outID))
+  atom.nc_mode("enable")
+else   
+  atom.message("\nERROR: Unable to locate an ATOM Pad Controller\n") 
+end
+
+
+
